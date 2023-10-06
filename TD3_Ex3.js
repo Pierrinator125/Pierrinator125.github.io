bf6c1c193b78da2b604c798d79692b6deb493a4b
Pierrinator125 <99200847+Pierrinator125@users.noreply.github.com>
2023-10-06 17:45:20 +0200

    TD3

--- TD3_Ex3.js
+++ TD3_Ex3.js
@@ -1,4 +1,6 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
 
 const scene = new THREE.Scene();
 var aspect = window.innerWidth/window.innerHeight;
@@ -8,18 +10,38 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const geometry = new THREE.BoxGeometry( 2, 2, 2 );
+const material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
+
+const texture = new THREE.TextureLoader().load( "PierreCarré.png" );
+texture.wrapS = THREE.RepeatWrapping;
+texture.wrapT = THREE.RepeatWrapping;
+
+material.map = texture
+
+const loader = new GLTFLoader();
+
+loader.load( 'potted_plant_01_4k.gtlf', function ( gltf ) {
+
+	scene.add( gltf.scene );
+
+}, undefined, function ( error ) {
+
+	console.error( error );
+
+} );
 
 camera.position.z = 5;
 
 var render = function () {
     requestAnimationFrame( render );
-    cube.rotation.x += 0.1;
-    cube.rotation.y += 0.1;
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    
     renderer.render( scene, camera );
   };
   
+
 render();
